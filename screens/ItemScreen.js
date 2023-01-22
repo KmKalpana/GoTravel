cd66e47cfe9c6079469e2c8d34c7139d4b5dcd07
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2023-01-22 15:49:03 +0530

    Setup navigation

---
+++ screens/ItemScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const ItemScreen = () => {
+    return (
+        <View>
+            ItemScreen
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default ItemScreen;
